85b2dc25f624df8cf2f08d69b4c23fb7298126dd
Joao Piologo <joaotpiologo@hotmail.com>
2024-08-05 20:22:45 -0300

    first commit

---
+++ src/components/Logo/index.js
@@ -0,0 +1,13 @@
+import './estilo.css'
+import logo from '../../images/logo.svg'
+
+function Logo(){
+    return (
+        <div className='logo'>
+            <img src={logo} alt='Logo do Website'></img>
+            <p><strong>Alura</strong>Books</p>
+        </div>
+    )
+}
+
+export default Logo;
